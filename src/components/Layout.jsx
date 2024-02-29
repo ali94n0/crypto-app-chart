57bea2c8e9e44149e7ec9e85a2bddb743b2a0a1d
ali94n0 <ali94n0@gmail.com>
2024-02-29 17:06:32 +0330

    layout done

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,11 +3,13 @@ import { Toaster } from "react-hot-toast";
 
 const Layout = ({children}) => {
     return (
-        <div>
+        <div className="container mx-auto ">
             <Toaster/>
-            <header>header</header>
+            <header className=" flex items-center justify-center text-slate-400 bg-neutral-700 py-2 px-4 rounded-md font-bold ">
+                <h1>Crypto App</h1>
+            </header>
             {children}
-            <footer>footer</footer>
+            <footer className=" flex items-center justify-center text-slate-400 bg-neutral-700 py-2 px-4 rounded-md font-bold mt-6">GitHub:&nbsp;<a className="font-normal text-sm text-blue-700" href="https://github.com/ali94n0">ali94n0</a></footer>
             
         </div>
     );
